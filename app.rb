fda2e5157a7c5402affb39ca02fb5447b203b9df
kellyjovan <jovanjkelly@yahoo.com>
2015-01-15 20:54:12 -0500

    Added imgs and fixed personality layout

--- app.rb
+++ app.rb
@@ -14,29 +14,63 @@ class MyApp < Sinatra::Base
 
   post '/questions' do
     score = Fruits_score.new()
-    score.name = params[:name]
+    @name = params[:name]
 
-    def score_each
-      params.each do |param|
-        
+    def score_each(score)
+      apple_choices  = ["red", "optimist", "unicorn", "sweet", "action", "math", "fire_fighter", "cheap", "laser", "average", "read"]
+      lime_choices   = ["green", "optimist", "godzilla", "sour", "horror", "science", "wrestler", "fast", "flight", "extreme", "sports"]
+      orange_choices = ["orange", "pessimist", "king_kong", "sweet", "romance", "ela", "teacher", "broken", "door", "worthless", "bully"]
+      grape_choices  = ["purple", "pessimist", "willy", "sour", "comedy", "social", "musician", "truck", "invisibility", "cool", "chill"]
+
+      params.each do |k,v|
+        if apple_choices.include?(v) && lime_choices.include?(v)
+          puts "Apple n Lime"
+          score.apple_point
+          score.lime_point
+        elsif orange_choices.include?(v) && grape_choices.include?(v)
+          puts "Orange n Grape"
+          score.orange_point
+          score.grape_point
+        elsif apple_choices.include?(v)
+          puts "Apple"
+          score.apple_point
+        elsif lime_choices.include?(v)
+          # score.lime_point
+          puts "Lime"
+          score.lime_point
+        elsif orange_choices.include?(v)
+          # score.orange_point
+          puts "Orange"
+          score.orange_point
+        elsif grape_choices.include?(v)
+          # score.grape_point
+          puts "Grape"
+          score.grape_point
+        end
+      puts "Key: #{k}"
+      puts "Value: #{v}"
       end
+      # puts "Apple: #{score.apple}"
+      # puts "Lime: #{score.lime}"
+      # puts "Orange: #{score.orange}"
+      # puts "Grape: #{score.grape}"
     end
 
-    score_each
+    score_each(score)
 
-    if params[:fav_color] == "red"
-      score.apple_point
-      puts score.apple
-    elsif params[:fav_color] == "green"
-      puts score.apple
-      score.lime_point
-    elsif params[:fav_color] == "orange"
-      puts score.apple
-      score.orange_point
-    elsif params[:fav_color] == "purple"
-      puts score.apple
-      score.grape_point
-    end
+    # if params[:fav_color] == "red"
+    #   score.apple_point
+    #   puts score.apple
+    # elsif params[:fav_color] == "green"
+    #   puts score.apple
+    #   score.lime_point
+    # elsif params[:fav_color] == "orange"
+    #   puts score.apple
+    #   score.orange_point
+    # elsif params[:fav_color] == "purple"
+    #   puts score.apple
+    #   score.grape_point
+    # end
 
     if score.apple > score.lime && score.apple > score.orange && score.apple > score.grape
       puts score.apple
